e1082f874bcfac3d058bc5ab8d47e069c3bce47d
matthewsmkim <forme3328@naver.com>
2022-11-12 20:19:32 +0900

    Add and Modify some fields in Member, and Divide authorities for CreateMember. (+ Modify Mapping URL in MemberController)

--- Vongole/src/main/java/com/simbongsa/Backend/entity/Member.java
+++ Vongole/src/main/java/com/simbongsa/Backend/entity/Member.java
@@ -2,10 +2,7 @@ package com.simbongsa.Backend.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.simbongsa.Backend.shared.Authority;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -30,14 +27,14 @@ public class Member extends Timestamped {
     @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "member_image")
-    private String memberImage;
+    @Column(name = "profile_image")
+    private String profileImage;
 
     @Column(name = "email")
     private String email;
 
-    @Column(name = "phone_num")
-    private String phoneNum;
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
     @Column(name = "name")
     private String name;
@@ -55,16 +52,25 @@ public class Member extends Timestamped {
     @Enumerated(EnumType.STRING)
     private Authority authority;
 
+    @Column(name = "license_number")
+    private String licenseNumber;
+
+    @Column(name = "license_image")
+    private String licenseImage;
+
+    @Column(name = "user_type")
+    private int userType;
+
 
     @Column(unique = true)
     private Long kakaoId;
+
+
+
 
 
     public void setKakaoId(Long kakaoId){
         this.kakaoId = kakaoId;
     }
 
-    public void updateMember() {
-
-    }
 }
