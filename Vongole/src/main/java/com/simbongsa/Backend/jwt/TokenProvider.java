a6505bd1ec0d5f800def3e71ed54506e2663d89b
K-SIIK <wind_breath@naver.com>
2022-11-14 14:41:30 +0900

    Edit : extend the expiration of refresh token

--- Vongole/src/main/java/com/simbongsa/Backend/jwt/TokenProvider.java
+++ Vongole/src/main/java/com/simbongsa/Backend/jwt/TokenProvider.java
@@ -39,8 +39,7 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "Authorization";
     private static final String BEARER_TYPE = "bearer ";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; //30분
-
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24; // 하루
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 7일
 
     private final MemberRepository memberRepository;
