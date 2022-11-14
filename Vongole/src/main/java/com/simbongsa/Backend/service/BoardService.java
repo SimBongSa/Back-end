0732a1d9f09c6cc8f5cc716abfa0913765977837
K-SIIK <wind_breath@naver.com>
2022-11-14 12:33:57 +0900

    test commit

--- Vongole/src/main/java/com/simbongsa/Backend/service/BoardService.java
+++ Vongole/src/main/java/com/simbongsa/Backend/service/BoardService.java
@@ -56,6 +56,7 @@ public class BoardService {
         for (Board board : boards) {
             boardResponses.add(new BoardResponse(board));
         }
+        // test
 
         return ResponseDto.success(boardResponses);
     }
