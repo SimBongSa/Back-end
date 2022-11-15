8e2430ccfcc322f78947c3d0e0de5e9b5636bc90
K-SIIK <wind_breath@naver.com>
2022-11-15 22:37:06 +0900

    Edit : getBoards

--- Vongole/src/main/java/com/simbongsa/Backend/repository/BoardRepository.java
+++ Vongole/src/main/java/com/simbongsa/Backend/repository/BoardRepository.java
@@ -15,7 +15,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     // 게시물 작성한 유저인지 확인
     Optional<Board> findByMember(Member member);
 
-
     // 내가 작성한 게시물 조회
     List<Board> findAllByMember(Member member);
 }
