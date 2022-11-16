d48e7ca5b07bcc8a287bb29625ad65bf3766d2b0
K-SIIK <wind_breath@naver.com>
2022-11-16 20:52:06 +0900

    Edit : boardRequest

--- Vongole/src/main/java/com/simbongsa/Backend/dto/request/BoardRequest.java
+++ Vongole/src/main/java/com/simbongsa/Backend/dto/request/BoardRequest.java
@@ -6,32 +6,34 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 public class BoardRequest {
 
-    @NotBlank(message = "빈 칸을 채워 주세요.")
+    @NotEmpty(message = "빈 칸을 채워 주세요.")
     private String title;
 
-    @NotBlank(message = "빈 칸을 채워 주세요.")
+    @NotEmpty(message = "빈 칸을 채워 주세요.")
     private String content;
 
     private MultipartFile boardImage;
 
-    @NotBlank(message = "봉사 활동 날짜를 정해 주세요.")
+    @NotEmpty(message = "봉사 활동 날짜를 정해 주세요.")
     private String dueDay;
 
-    @NotBlank(message = "지원 시작 날짜를 정해 주세요.")
+    @NotEmpty(message = "지원 시작 날짜를 정해 주세요.")
     private String startDate;
 
-    @NotBlank(message = "지원 마감 날짜를 정해 주세요.")
+    @NotEmpty(message = "지원 마감 날짜를 정해 주세요.")
     private String endDate;
 
-    @NotBlank(message = "봉사 활동 위치를 정해 주세요.")
+    @NotEmpty(message = "봉사 활동 위치를 정해 주세요.")
     private String area;
 
-    @NotBlank(message = "상세 주소를 작성해 주세요.")
+    @NotEmpty(message = "상세 주소를 작성해 주세요.")
     private String detailArea;
 
 //    @NotBlank(message = "봉사 활동 종류를 선택해 주세요.")
