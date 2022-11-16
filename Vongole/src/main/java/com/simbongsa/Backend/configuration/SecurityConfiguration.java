f6a67056033aa4efcb88e72575e91916299c6808
QBishop <koresy001@gmail.com>
2022-11-16 14:33:40 +0900

    댓글 사용 수정 완료

--- Vongole/src/main/java/com/simbongsa/Backend/configuration/SecurityConfiguration.java
+++ Vongole/src/main/java/com/simbongsa/Backend/configuration/SecurityConfiguration.java
@@ -54,7 +54,7 @@ public class SecurityConfiguration {
                 .antMatchers("/members/**").permitAll()
                 .antMatchers("/boards").permitAll()
                 .antMatchers("/boards/**").permitAll() // boards 이후의 모든 엔드포인트에 대해 허용
-                .antMatchers("/comments/*").permitAll()
+//                .antMatchers("/comments/*").permitAll()
                 .antMatchers("/user/kakao/callback").permitAll()
                 .anyRequest().authenticated()
 
