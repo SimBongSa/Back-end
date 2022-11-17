86d2de4b1a780bccf37058a55c2472720a32d8c7
matthewsmkim <forme3328@naver.com>
2022-11-17 16:30:03 +0900

    Combine createEnrollment & cancelEnrollment with 'enrollApprovalAndCancel' and Modify 'String birthdate' -> 'LocalDate birthdate'.

--- Vongole/src/main/java/com/simbongsa/Backend/controller/EnrollController.java
+++ Vongole/src/main/java/com/simbongsa/Backend/controller/EnrollController.java
@@ -6,7 +6,6 @@ import com.simbongsa.Backend.entity.UserDetailsImpl;
 import com.simbongsa.Backend.service.EnrollService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +16,10 @@ public class EnrollController {
 
     private final EnrollService enrollService;
 
-    @PostMapping("/boards/{board_id}/ask")
-    public ResponseDto<MsgResponse> createEnrollment(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                                    @PathVariable("board_id") Long boardId) {
-        return enrollService.createEnrollment(userDetails.getMember(), boardId);
+    @PostMapping("/boards/{board_id}/apply")
+    public ResponseDto<MsgResponse> enrollApprovalAndCancel(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                            @PathVariable("board_id") Long boardId) {
+        return enrollService.enrollApprovalAndCancel(userDetails.getMember(), boardId);
     }
 
-    @DeleteMapping("/boards/{board_id}/ask")
-    // Todo 봉사 활동 신청 시 리스펀스로 enrollId를 주는 게 아닌데 어떻게 pathVariable 로 받는지?
-    // Todo pathVariable 로 boardId 안받음
-    public ResponseDto<MsgResponse> cancelEnrollment(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                                     @PathVariable("board_id") Long boardId) {
-        return enrollService.cancelEnrollment(userDetails.getMember(), boardId);
-
-    }
 }
